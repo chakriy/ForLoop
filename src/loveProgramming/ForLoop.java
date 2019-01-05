fa67b136974491132fed6f83c850504125cfbde1
chakriy <ychakri@yahoo.com>
2019-01-05 14:52:42 +0000

    Java ForLoop

---
+++ src/loveProgramming/ForLoop.java
@@ -0,0 +1,9 @@
+package loveProgramming;
+
+class ForLoop {
+    public static void main(String args[]){
+        for(int a=50; a>0; a--){
+            System.out.println("The value of a is: "+a);
+        }
+    }
+}
